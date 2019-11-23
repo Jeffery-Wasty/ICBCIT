4c846537b1b99e090929b7aa8c3dde3b08678283
Henry (Local) <henryjiang@live.ca>
2019-11-23 15:57:23 -0800

    changing rf classify with slightly better skewed training

--- randomforestclassify.py
+++ randomforestclassify.py
@@ -4,40 +4,6 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 import random
 
-def randomforest_kfoldCV(x, y, K, n):
-    subset_in = np.array_split(x, K)
-    subset_out = np.array_split(y, K)
-    incorrect_arr = []
-    for i in range(K):
-        incorrect = 0
-        validation_setin = subset_in[i]
-        validation_setout = subset_out[i]
-        if (i == 0):
-            training_setin = np.concatenate(subset_in[1:])
-            training_setout = np.concatenate(subset_out[1:])
-        elif (i == K - 1):
-            training_setin = np.concatenate(subset_in[0:i])
-            training_setout= np.concatenate(subset_out[0:i])
-        else:
-            training_setin = np.concatenate(subset_in[0:i] + subset_in[i + 1:])
-            training_setout = np.concatenate(subset_out[0:i] + subset_out[i + 1:])
-
-        clf = RandomForestClassifier(n_estimators=100, max_features=5, max_leaf_nodes=n)
-        clf.fit(training_setin, training_setout)
-        predicted = clf.predict(validation_setin)
-
-        validation_setout = np.array(validation_setout)
-
-        for j in range(len(validation_setout)):
-            if predicted[j] != validation_setout[j]:
-                incorrect = incorrect + 1
-        incorrect_arr.append(incorrect / len(validation_setout))
-#        print('Acc:', metrics.accuracy_score(validation_setout, predicted))
-
-    cv_error = sum(incorrect_arr) / K
-    print('Acc for :', n, "features", cv_error)
-    return cv_error
-
 
 def evaluate(model, test_features, test_labels):
     predictions = model.predict(test_features)
@@ -50,17 +16,18 @@ def evaluate(model, test_features, test_labels):
 
     return accuracy
 
-test_csv = pd.read_csv('testset.csv')
+# loading csv's
+# test_csv = pd.read_csv('testset.csv')
 data = pd.read_csv('trainingset.csv')
 
-original_data = data.copy()
-
+# Change ClaimAmount to 0's and 1's for whether they claimed or not.
 for i, row in data.iterrows():
     claim_val = 0
     if data.at[i, 'ClaimAmount'] > 0:
         claim_val = 1
     data.at[i, 'ClaimAmount'] = claim_val
 
+# 75 to 25 ratio
 train_ratio = 0.75
 num_rows = data.shape[0]
 train_set_size = int(train_ratio * num_rows)
@@ -72,42 +39,48 @@ random.shuffle(shuffled_indices)
 train_indices = shuffled_indices[:train_set_size]
 test_indices = shuffled_indices[train_set_size:]
 
-original_data = original_data.iloc[train_indices, :]
 train_data = data.iloc[train_indices, :]
 test_data = data.iloc[test_indices, :]
 
+# Filter out 0's in the test data, comment out if want to use entire training set.
 indexNames = test_data[ test_data['ClaimAmount'] != 1 ].index
-test_data.drop(indexNames, inplace=True)
+test_data = test_data.drop(indexNames, inplace=False)
 
+# Obtain training rows with claims
 indexNames = train_data[ train_data['ClaimAmount'] != 1 ].index
 train_claims = train_data.drop(indexNames, inplace=False)
 
+# Obtain training rows without claims
 indexNames = train_data[ train_data['ClaimAmount'] != 0 ].index
 train_no_claims = train_data.drop(indexNames, inplace=False)
-no_claims_rows = train_no_claims.shape[0]
-no_claims_size = int(0.01 * no_claims_rows)
 
+no_claims_rows = train_no_claims.shape[0]
+no_claims_size = int(0.05 * no_claims_rows)
+
+print("Using", train_claims.shape[0], "claims")
+print("Using", no_claims_size, "non-claims")
+
+# Create training set with specific amount of 1's and 0's
 train_data = train_claims.append(train_no_claims[:no_claims_size])
 
 num_rows = train_data.shape[0]
 
 shuffled_indices = list(range(num_rows))
+# Use for random shuffling, can comment out if using k-fold cv
 random.seed(42)
 random.shuffle(shuffled_indices)
 
 train_indices = shuffled_indices[:num_rows]
 train_data = train_data.iloc[train_indices, :]
 
-print(train_data.head(50).to_string())
+# Filter out certain columns
+training_data_in = train_data.loc[:, ['feature3', 'feature4', 'feature5', 'feature7',
+                                      'feature9', 'feature11', 'feature13', 'feature14',
+                                      'feature15', 'feature16', 'feature18', 'ClaimAmount']]
 
-training_data_in = train_data.loc[:, ['feature4', 'feature9', 'feature13', 'feature14', 'feature15', 'feature16',
-                                      'feature18', 'ClaimAmount']]
-
-test_data_in = test_data.loc[:, ['feature4', 'feature9', 'feature13', 'feature14', 'feature15', 'feature16',
-                                      'feature18', 'ClaimAmount']]
-
-true_test = test_csv.loc[:, ['feature4', 'feature9', 'feature13', 'feature14', 'feature15', 'feature16',
-                                      'feature18']]
+test_data_in = test_data.loc[:, ['feature3', 'feature4', 'feature5', 'feature7',
+                                      'feature9', 'feature11', 'feature13', 'feature14',
+                                      'feature15', 'feature16', 'feature18', 'ClaimAmount']]
 
 training_data_in = training_data_in.drop('ClaimAmount', axis=1, inplace=False)
 training_data_out = train_data.loc[:, 'ClaimAmount']
@@ -115,60 +88,18 @@ training_data_out = train_data.loc[:, 'ClaimAmount']
 test_data_in = test_data_in.drop('ClaimAmount', axis=1, inplace=False)
 test_data_out = test_data.loc[:, 'ClaimAmount']
 
-#print("Error cv:", sklearn_kNN_kfoldCV(training_data_in, training_data_out, 5, 11))
-
-# Number of trees in random forest
-n_estimators = [int(x) for x in np.linspace(start = 100, stop = 1000, num = 10)]
-# Number of features to consider at every split
-max_features = ['auto', 'sqrt']
-# Maximum number of levels in tree
-max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
-max_depth.append(None)
-# Minimum number of samples required to split a node
-min_samples_split = [2, 5, 10]
-# Minimum number of samples required at each leaf node
-min_samples_leaf = [1, 2, 4]
-# Method of selecting samples for training each tree
-bootstrap = [True, False]
-# Create the random grid
-random_grid = {'n_estimators': n_estimators,
-               'max_features': max_features,
-               'max_depth': max_depth,
-               'min_samples_split': min_samples_split,
-               'min_samples_leaf': min_samples_leaf,
-               'bootstrap': bootstrap}
-
 clf = RandomForestClassifier(n_estimators=100)
-#rf_random = RandomizedSearchCV(estimator = clf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
 clf.fit(training_data_in, training_data_out)
-
-#y_true_test = clf.predict(true_test)
 
 y_pred = clf.predict(test_data_in)
 y_train = clf.predict(training_data_in)
 
-#print(rf_random.best_params_)
-
-#best_random = rf_random.best_estimator_
-#random_accuracy = evaluate(best_random, test_data_in, test_data_out)
-
-#ks = np.arange(2, 15).tolist()
-#prediction_array = []
-
-#for i in range(len(ks)):
-#    predictions = randomforest_kfoldCV(training_data_in, training_data_out, 10, ks[i])
-#    prediction_array.append(predictions)
-
-#plt.plot(ks, prediction_array)
-#plt.suptitle("Average cross-prediction error against number of neighbours checked")
-#plt.xlabel("k = ")
-#plt.ylabel("Average cross-prediction error")
-#plt.legend(['y = avg prediction error'], loc='upper left')
-#plt.show()
-
+# Print accuracy of 'test' and 'training'
 
 print('Acc:', metrics.accuracy_score(test_data_out, y_pred))
 print('2nd Acc:', metrics.accuracy_score(training_data_out, y_train))
+
+# Code to export dataframe
 
 #export = test_csv.copy()
 #export['PredictedCategory'] = y_true_test
